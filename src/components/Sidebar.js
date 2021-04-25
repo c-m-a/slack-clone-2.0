31dfc81a5d30b93d472d7811de137de9d750b9cd
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-04-25 17:49:31 -0500

    Add styles for the side bar header section

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 
 export default function Sidebar() {
   return (
@@ -12,6 +13,7 @@ export default function Sidebar() {
             Cma
           </h3>
         </SidebarInfo>
+        <CreateIcon />
       </SidebarHeader>
     </SidebarContainer>
   );
@@ -21,9 +23,50 @@ const SidebarContainer = styled.div`
   width: 26rem;
   height: 100%;
   color: var(--sidebar-color);
-  background-color: var(--sidebar-bg);
+  background-color: var(--sidebar-bg-color);
 `;
 
-const SidebarHeader = styled.div``;
+const SidebarHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+  padding: 1.3rem;
+  border-top: .1rem var(--sidebar-header-border-color) solid;
+  border-bottom: .1rem var(--sidebar-header-border-color) solid;
 
-const SidebarInfo = styled.div``;
+  :hover {
+    background-color: var(--slack-color);
+  }
+
+  > .MuiSvgIcon-root {
+    padding: .8rem;
+    color: var(--sidebar-bg-color);
+    font-size: 3rem;
+    background-color: white;
+    border-radius: 50%;
+  }
+`;
+
+const SidebarInfo = styled.div`
+  flex: 1;
+
+  > h2 {
+    font-size: 1.5rem;
+    font-weight: 900;
+    margin-bottom: .5rem;
+  }
+
+  > h3 {
+    display: flex;
+    font-size: 1.3rem;
+    font-weight: 400;
+    align-items: center;
+  }
+
+  > h3 > .MuiSvgIcon-root {
+    font-size: 1.4rem;
+    margin-top: .1rem;
+    margin-right: .2rem;
+    color: var(--online-color);
+  }
+`;
